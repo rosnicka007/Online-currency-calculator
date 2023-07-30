a21e41e57fa4d870b652f791c769a6ebac0cb97a
rosnicka007 <120189020+rosnicka007@users.noreply.github.com>
2023-07-30 17:49:08 +0200

    translation of comments

--- currency_calculator.py
+++ currency_calculator.py
@@ -2,11 +2,11 @@ from tkinter import *
 import requests
 import time
 
-# Barvy
+# Color setting
 main_bg_color = "#ffb277"
 main_fg_color = "#170a00"
 
-# Okno
+# Window setting
 window = Tk()
 window.minsize(350, 200)
 window.title("Přepočet měn")
@@ -14,7 +14,7 @@ window.resizable(False, False)
 #window.iconbitmap("mesec.ico")
 window.config(bg= main_bg_color)
 
-# Funkce
+# Functions
 def count():
     try:
 
@@ -41,58 +41,58 @@ def count():
     except:
         notification_label.config(text="Zadejte prosím částku")
 
-# Uživatelský vstup
+# User input
 user_input = Entry(width=10, font=("Arial", 12), justify=CENTER)
 user_input.focus()
 user_input.grid(row=0, column=0, padx=10, pady=(10, 0))
 
-# Roletka - z jaké měny
+# Drop down - selecting input currency 
 drop_down_from = StringVar(window)
 drop_down_from.set("CZK") # výchozí hodnota
 drop_down_from_option = OptionMenu(window, drop_down_from, "AUD", "CHF", "CZK", "EUR", "GBP","USD")
 drop_down_from_option.grid(row=0, column=1, padx=10, pady =(10, 0))
 
-# Roletka - na jakou měnu
+# Drop down - selecting output currency
 drop_down_to = StringVar(window)
 drop_down_to.set("CZK")
 drop_down_to_option = OptionMenu (window, drop_down_to, "AUD", "CHF", "CZK", "EUR", "GBP","USD" )
 drop_down_to_option.grid(row=1, column=1, padx=10, pady=(10, 0))
 
-# Tlačítko přepočtu
+# Button for conversion
 count_button = Button(text="Přepočítat", font=("Arial", 12), command=count)
 count_button.grid(row=0, column=2, padx=10, pady=(10, 0))
 
-# Label - výsledek převodu
+# Label with result of conversion
 result_label = Label (text="0", bg=main_bg_color, fg=main_fg_color, font =("Arial", 12, "bold"), )
 #result_label = str(result_label).replace(".", ",")
 
 result_label.grid(row=1, column=0, pady=(10,0))
 
-# Label - upozornění
+# Label for notification
 notification_label = Label (bg=main_bg_color, fg=main_fg_color, font=("Arial", 12))
 notification_label.grid(row=2, column=0)
 
-# Label - kurz
+# Label for course rate
 rate_label = Label (text="Aktuální kurz:", bg=main_bg_color,  fg=main_fg_color, font=("Arial", 12))
 
 rate_label.grid(row=3, column=0, sticky="w", padx = 10)
 
-# Label - aktuální kurz
+# Label for actual course rate
 current_rate_label = Label (bg=main_bg_color, fg=main_fg_color, font=("Arial", 12))
 
 current_rate_label.grid(row=3, column=1, sticky="w", padx=12)
 
-# Label - datum
+# Label for date
 date_label = Label (bg=main_bg_color, text="Datum: ", fg=main_fg_color, font=("Arial", 12), )
 date_label.grid(row=4, column=0, sticky="w", padx = 10)
 
-# Datum
+# Actual date
 local= time.localtime()
 
 
-# Label - datum transakce
+# Label for date of transaction
 date_of_transaction_label = Label (text= "{}. {}. {}".format(local[2], local[1], local[0]), bg=main_bg_color,  fg=main_fg_color, font=("Arial", 12))
 date_of_transaction_label.grid(row=4, column=1, sticky="w", padx=12)
 
-# Hlavní cyklus
+# Main loop
 window.mainloop()
